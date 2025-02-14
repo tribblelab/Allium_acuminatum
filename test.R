c92bcf515d17729455a406f58e737d4b32d1bf29
Hayden Wright <haydenwright@Haydens-MacBook-Air-4.local>
2025-02-14 08:57:17 -0800

    Edited test.R Added script file nmds_test.R

--- test.R
+++ test.R
@@ -4,6 +4,10 @@ library(ggplot2)
 
 # Imports Allium data
 Allium_data <- read.csv('/Users/haydenwright/Desktop/R_files/Allium_acuminatum/Allium_data.csv')
+# Creates subset of Allium_data that will be normalized
+Allium_data_norm <- Allium_data[-c(1:5,7,10:22,25,28:34,36,40:42,45:51,54)]
+# makes any negative values positive in the subset to be normalized
+Allium_data_norm <- abs(Allium_data_norm)
 
 # Splits Allium data into within range and outside of range
 within_range <- subset(Allium_data,within_lat_long_range==1)
@@ -33,3 +37,31 @@ ovary_cresting <- chisq.test(table(Allium_data$within_lat_long_range,Allium_data
 results <- rbind(results,ovary_cresting)
 ovary_long_projections <- chisq.test(table(Allium_data$within_lat_long_range,Allium_data$ovary_long_projections), correct=FALSE)
 results <- rbind(results,ovary_long_projections)
+
+# Function to min-max normalize data
+normalize <- function(x) {
+  ((x - min(x,na.rm = TRUE)) / (max(x,na.rm = TRUE) - min(x,na.rm = TRUE)))
+}
+
+# Scales all the data in Allium_data_norm
+Allium_data_norm <- as.data.frame(lapply(Allium_data_norm, normalize))
+
+# Runs a PCA analysis
+results <- prcomp(na.exclude(Allium_data_norm), scale. = TRUE)
+results$rotation <- -1*results$rotation
+results$x <- -1*results$x
+
+
+temp <- lapply(Allium_data$tepal_inner_outer_ratio_avg[!is.na(Allium_data$tepal_inner_outer_ratio_avg)]
+, min_max_norm)
+
+normalized_allium_data <- Allium_data
+normalized_allium_data$tepal_inner_outer_ratio_avg <- normalize(Allium_data$tepal_inner_outer_ratio_avg)
+
+(Allium_data$tepal_inner_outer_ratio_avg - min(Allium_data$tepal_inner_outer_ratio_avg[!is.na(Allium_data$tepal_inner_outer_ratio_avg)])) / (max(Allium_data$tepal_inner_outer_ratio_avg[!is.na(Allium_data$tepal_inner_outer_ratio_avg)]) - min(Allium_data$tepal_inner_outer_ratio_avg[!is.na(Allium_data$tepal_inner_outer_ratio_avg)]))
+
+(Allium_data[is.numeric(Allium_data)] - min(Allium_data[is.numeric(Allium_data)]) / (max(Allium_data[is.numeric(Allium_data)] - min(Allium_data[is.numeric(Allium_data)])
+
+library("vegan")
+cascadeKM(na.exclude(Allium_data_norm), inf.gr = 2, sup.gr = 8)
+plot(cascadeKM(na.exclude(Allium_data_norm), inf.gr = 2, sup.gr = 8))
